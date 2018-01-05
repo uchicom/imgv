b7fedab50aabd98c46a9fc2a93b1646a0bbfd98b
Shigeki Uchiyama <shigeki.uchiyama@uchicom.com>
2018-01-05 18:27:15 +0900

    コメント検証

--- src/main/java/com/uchicom/imgv/Main.java
+++ src/main/java/com/uchicom/imgv/Main.java
@@ -7,9 +7,18 @@ import java.util.Iterator;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageTypeSpecifier;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.stream.ImageOutputStream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import com.github.jaiimageio.jpeg2000.J2KImageWriteParam;
 
@@ -36,19 +45,63 @@ public class Main {
 
 	public static void lossless() {
 		try {
-			BufferedImage image = ImageIO.read(new File("sample/jio.png"));
+			BufferedImage image = ImageIO.read(new File("sample/turkey-3048299_1920.jpg"));
 			File f = new File("test-jpeg2000-lossless.jp2");
 			Iterator<ImageWriter> writers = ImageIO.getImageWritersBySuffix("jp2");
 			ImageWriter writer = writers.next();
 			J2KImageWriteParam writeParams = (J2KImageWriteParam) writer.getDefaultWriteParam();
 			writeParams.setLossless(true);
 			// writeParams.setFilter(J2KImageWriteParam.FILTER_53);
-			// writeParams.setEncodingRate(64.0f);
-			 writeParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-			 writeParams.setCompressionType("JPEG2000");
-			 writeParams.setCompressionQuality(0.48f);
-			 writeParams.setTiling(1024, 1024, 0, 0);
+			//			 writeParams.setEncodingRate(64.0f);
+			writeParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+			writeParams.setCompressionType("JPEG2000");
+			writeParams.setCompressionQuality(0.02f);
+			writeParams.setTiling(1024, 1024, 0, 0);
+			writeParams.setProgressionType("res");
 
+//			IIOMetadata imageMeta = writer.getDefaultImageMetadata(new ImageTypeSpecifier(image), writeParams);
+//			DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+//			DocumentBuilder docbuilder = dbfactory.newDocumentBuilder(); // DocumentBuilderインスタンス
+//			Document document = docbuilder.newDocument(); // Documentの生成
+//			for (String name : imageMeta.getMetadataFormatNames()) {
+//				System.out.println(name);
+//			}
+//			Element root = document.createElement("com_sun_media_imageio_plugins_jpeg2000_image_1.0");
+//			Element tree = (Element) imageMeta.getAsTree("com_sun_media_imageio_plugins_jpeg2000_image_1.0");
+//			NodeList nodeList =  tree.getChildNodes();
+//			for (int i = 0;i < nodeList.getLength(); i++) {
+//				Node child  = nodeList.item(i);
+//				System.out.println(child.getNodeName());
+//				NodeList childList =  child.getChildNodes();
+//				for (int j = 0;j < childList.getLength(); j++) {
+//					Node cc  = childList.item(j);
+//					System.out.println(" " + cc.getNodeName());
+//				}
+//			}
+//			tree = (Element) imageMeta.getAsTree("javax_imageio_1.0");
+//			System.out.println(tree.getOwnerDocument());
+//			nodeList =  tree.getChildNodes();
+//			for (int i = 0;i < nodeList.getLength(); i++) {
+//				Node child  = nodeList.item(i);
+//				System.out.println(child.getNodeName());
+//				NodeList childList =  child.getChildNodes();
+//				for (int j = 0;j < childList.getLength(); j++) {
+//					Node cc  = childList.item(j);
+//					System.out.println(" " + cc.getNodeName());
+//				}
+//			}
+//			tree = document.createElement("javax_imageio_jpeg_image_1.0");
+//			Element elem = document.createElement("markerSequence");
+//			tree.appendChild(elem);
+//			Element elem2 = document.createElement("com");
+//			elem.appendChild(elem2);
+//			Element comment = document.createElement("Comment");
+//			comment.appendChild(document.createTextNode("Copyright: National Diet Library, Japan"));
+//			elem2.appendChild(comment);
+//			//			Element com = (Element) tree.getElementsByTagName("Comment").item(0);
+//			//			com.setAttribute("com", "Copyright: National Diet Library, Japan"); // 解像度の単位をDPIにする。
+//			// メタデータを設定
+//			imageMeta.setFromTree("com_sun_media_imageio_plugins_jpeg2000_image_1.0", tree);
 			ImageOutputStream ios = ImageIO.createImageOutputStream(f);
 			writer.setOutput(ios);
 			writer.write(null, new IIOImage(image, null, null), writeParams);
